6b1429c2f92fdbdff1fc08d3b7ba2c30c060f156
Théo <tochampion38@gmail.com>
2019-05-16 21:19:28 +0200

    [App NodeJs] Mise en place du squelette du serveur Node
    - Installation d'Express (configuration).
    - Lien entre le serveur Express et le routeur index.js dans le dossier routes.
    - Initialisation de la connexion à la base de donnée locale de la librairie.
    
    - Mise en  forme du dossier ressources (pour le SCSS).
    - Ajout d'autres partials (header, footer).

---
+++ node_application/routes/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const axios = require('axios');
+const mysql = require('mysql');
+
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('pages/index');
+    });
+
+    app.post('/', function (req, res) {
+        //Retrieve data from form
+        let category = req.body.category;
+        let table = req.body.table;
+
+        //Instance local database connection 
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'infomaniak'
+        });
+        connection.connect();
+
+        //Prepare queries for database
+        let retrieveCurrentBookStored = `SELECT * FROM ${table}`;
+
+
+        // 1) First step, retrieve current books stored in database
+
+
+        // 2) Retrieve data from GoogleBook API in relation to category send from form
+
+        res.render('pages/validation');
+    });
+};
---
+++ node_application/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//Load all dependencies
+var express = require('express');
+var routes = require('./routes/index.js');
+var port = process.env.PORT || 3000;
+var app = express();
+
+
+//Config the Node server
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+
+//Load router
+routes(app);
+
+
+//Lauch the server
+app.listen(port, function() {
+ console.log(`Server listening on http://localhost:${port}`);
+});
