cd06e5b7a5f80318e014cbcb9d10203668ae36aa
Théo <tochampion38@gmail.com>
2019-05-17 02:54:56 +0200

    [App NodeJs + Library] Migration table category + avancement App NodeJs
    [Library]
    Migration de la table category pour dynamiser l'app NodeJs
    
    [App NodeJs]
    Etape 1 et 2 faites (récupérations des livres dans la BDD + requête axios sur l'API Google.

--- library_application/database/migrations/2019_05_16_220512_create_categories_table.php
+++ library_application/database/migrations/2019_05_16_220512_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('url_logo')->nullable();
             $table->timestamps();
         });
     }
--- node_application/routes/index.js
+++ node_application/routes/index.js
@@ -5,18 +5,15 @@ const mysql = require('mysql');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        let success = 'succès !!';
-        let error = '';
-        res.render('pages/index', {
-            success: success,
-            error: error,
-        });
+        res.render('pages/index');
     });
 
     app.post('/', function (req, res) {
         //Retrieve data from form
         let category = req.body.category;
         let table = req.body.table;
+        // let table = books;
+        // let category = PHP;
 
         //Instance local database connection 
         const connection = mysql.createConnection({
@@ -31,8 +28,36 @@ module.exports = function (app) {
         let retrieveCurrentBookStored = `SELECT * FROM ${table}`;
 
 
-        // 1) First step, retrieve current books stored in database
+        // 1) First step, retrieve current books stored in database in stock them in array (listTitleBookStored)
+        connection.query(retrieveCurrentBookStored, function (error, results, fields) {
+            if (error) throw error;
+            let listTitleBookStored = [];
+            results.forEach(element => {
+                listTitleBookStored.push(element.title);
+            });
+            
+            // 2) Second step, retrieve data from GoogleBook API in relation to category pass by form
+            axios.get(`https://www.googleapis.com/books/v1/volumes?q=${category}&maxResults=40`)
+             .then(function (response) {
+                 // Target book data from axios request
+                 let dataBooks = response.data.items;
 
+                 dataBooks.forEach(element => {
+
+                    let newdataBooks = {
+                        title: element.volumeInfo.title, 
+                        authors: element.volumeInfo.authors,
+                        url_thumbnail: element.volumeInfo.imageLinks.thumbnail,
+                        description: element.volumeInfo.description,
+                        pageCount: element.volumeInfo.pageCount,
+                        lang: element.volumeInfo.language,
+                        publishedDate: element.volumeInfo.publishedDate,
+                        price: element.saleInfo.retailPrice.price.amount,
+                    };
+                    console.log(newdataBooks);
+                 });
+             });
+        });
 
         // 2) Retrieve data from GoogleBook API in relation to category send from form
 
