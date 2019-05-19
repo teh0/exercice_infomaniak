8b504cb6f0ab9834f72075752f2eb8411206ce97
Théo <tochampion38@gmail.com>
2019-05-20 01:54:22 +0200

    [Library] Skin page profile + liste
    - Possibilité de consulter la liste des livres emprunter
    - Mot "livre" dynamique ( avec ou sans s en fonction de la conjugaison).

--- library_application/resources/views/users/profile.blade.php
+++ library_application/resources/views/users/profile.blade.php
@@ -6,33 +6,38 @@
     <section class="app-wrapper">
         {{-- User Infos --}}
         <div class="user-infos">
-            <p>Profil de {{ Auth::user()->name }}</p>
+            <p class="header-user-infos"><span class="sharp">#</span>Profil de {{ Auth::user()->name }}</p>
             {{-- Thumbnail user --}}
             <div class="img-profile-container"><img src="{{ asset('upload/avatars/'.Auth::user()->avatar) }}" alt="">
             </div>
             {{-- Form to change Thumbnail --}}
             <form enctype="multipart/form-data" action="{{ route('update_avatar') }}" method="POST">
                 @csrf
-                <input type="file" name="avatar">
+                <label for="avatar">Changer la photo</label>
+                <input id="avatar" type="file" name="avatar">
                 <input type="submit">
             </form>
         </div>
 
         {{-- List of borrowed books --}}
-        <div class="user-borrow-list">
-            @foreach (Auth::user()->books as $book)
-            <div class="block-borrow">
-                <span>{{ $book->title }}</span>
-                <a class="link-borrow" href=""
-                    onclick="event.preventDefault(); document.getElementById('unborrow-form').submit();">Rendre le
-                    livre</a>
-                <form id="unborrow-form" action="{{ route('unborrowBook', ['id_book' => $book->id]) }}" method="POST"
-                    style="display: none;">
-                    @csrf
-                </form>
+        <div class="user-borrow">
+            <p class="header-user-borrow"><span class="sharp">#</span> Vos Emprunts</p>
+            <div class="list-borrow-book">
+                @foreach (Auth::user()->books as $book)
+                <div class="block-borrow-book">
+                    <img src="{{ $book->small_thumbnail }}" alt="">
+                    <a class="link-borrow" href="" onclick="event.preventDefault(); document.getElementById('unborrow-form').submit();">Rendre le
+                        livre</a>
+                    <form id="unborrow-form" action="{{ route('unborrowBook', ['id_book' => $book->id]) }}" method="POST"
+                        style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+                <br>
+                @endforeach
             </div>
-            <br>
-            @endforeach
+        <p class="book-credit">Vous pouvez encore emprunter <span>{{ 3 - Auth::user()->countBooks }} @if ((3 - Auth::user()->countBooks) <= 1) livre @else livres @endif</span></p>
+            <a href="{{ route('collection') }}" class="back-button">Voir tous les livres</a>
         </div>
 
     </section>
